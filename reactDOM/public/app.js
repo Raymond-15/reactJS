b539e744062e4f300ce8b0350d22252cba57ec2b
Raymond-15 <raymondputra15@gmail.com>
2022-04-03 19:27:50 +0800

    simple todo list

--- reactDOM/public/app.js
+++ reactDOM/public/app.js
@@ -1,12 +1,47 @@
 const root = document.querySelector("#root");
 
 function App() {
-  const fruits = ["Apple", "Mango", "Berry", "Orange"];
-  return /*#__PURE__*/React.createElement("ul", null, fruits.map(function (fruit) {
+  const [aktivitas, setAktivitas] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+
+  function generateId() {
+    return Date.now();
+  }
+
+  function addTodoHandler(event) {
+    event.preventDefault();
+    setTodos([...todos, {
+      id: generateId(),
+      aktivitas: aktivitas
+    }]);
+    setAktivitas("");
+  }
+
+  function removeTodoHandler(todoId) {
+    const filteredTodos = todos.filter(function (todo) {
+      return todo.id !== todoId;
+    });
+    setTodos(filteredTodos);
+  }
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Todo list sederhana"), /*#__PURE__*/React.createElement("form", {
+    onSubmit: addTodoHandler
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    placeholder: "Masukkan aktivitas",
+    value: aktivitas,
+    onChange: function (event) {
+      setAktivitas(event.target.value);
+    }
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Tambah")), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
-      key: fruit
-    }, fruit);
-  }));
+      key: todo.id
+    }, todo.aktivitas, /*#__PURE__*/React.createElement("button", {
+      onClick: removeTodoHandler.bind(this, todo.id)
+    }, "Hapus"));
+  })));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- reactDOM/src/app.js
+++ reactDOM/src/app.js
@@ -1,14 +1,60 @@
 const root = document.querySelector("#root");
 
 function App() {
-  const fruits = ["Apple", "Mango", "Berry", "Orange"];
+  const [aktivitas, setAktivitas] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+
+  function generateId() {
+    return Date.now();
+  }
+
+  function addTodoHandler(event) {
+    event.preventDefault();
+
+    setTodos([
+      ...todos,
+      {
+        id: generateId(),
+        aktivitas: aktivitas,
+      },
+    ]);
+
+    setAktivitas("");
+  }
+
+  function removeTodoHandler(todoId) {
+    const filteredTodos = todos.filter(function (todo) {
+      return todo.id !== todoId;
+    });
+
+    setTodos(filteredTodos);
+  }
 
   return (
-    <ul>
-      {fruits.map(function (fruit) {
-        return <li key={fruit}>{fruit}</li>;
-      })}
-    </ul>
+    <>
+      <h1>Todo list sederhana</h1>
+      <form onSubmit={addTodoHandler}>
+        <input
+          type="text"
+          placeholder="Masukkan aktivitas"
+          value={aktivitas}
+          onChange={function (event) {
+            setAktivitas(event.target.value);
+          }}
+        />
+        <button type="submit">Tambah</button>
+      </form>
+      <ul>
+        {todos.map(function (todo) {
+          return (
+            <li key={todo.id}>
+              {todo.aktivitas}
+              <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
